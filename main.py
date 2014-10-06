f92ba80ed9bcbfd9579f0c13fb6139e5d652a5f6
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2014-10-06 14:15:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+class DellKeyboard(object):
+    def __init__(self):
+        self.make = "Dell"
+        self.model = "KB212-B"
+        self.color = "Black"
+        self.connection="none"
+
+    def printinfo(self):
+        print self.make
+        print self.model
+        print self.color
+        print self.connection
+
+    def connect(self, connection):
+        self.connection = connection
+
+class DellMouse(object):
+    def __init__(self):
+        self.make = "Dell"
+        self.model = "MS111-L"
+        self.color = "Black"
+        self.connection = "none"
+
+    def printinfo(self):
+        print self.make
+        print self.model
+        print self.color
+        print self.connection
+
+    def connect(self, connection):
+       self.connection = connection
+
+
+class DellMonitor(object):
+    def __init__(self):
+        self.make = "Dell"
+        self.model = "2209WAf"
+        self.size = "22 inch"
+        self.color = "Black & Silver"
+        self.connection="none"
+
+    def printinfo(self):
+        print self.make
+        print self.model
+        print self.size
+        print self.color
+        print self.connection
+
+    def connect(self, connection):
+        self.connection = connection
+
+
+class Workstation(object):
+    def __init__(self):
+        self.make = "Dell"
+        self.model = "Optiplex 7010"
+        self.color = "Black"
+        self.connection = "None"
+        self.owner="Nobody"
+        self.monitor1 = DellMonitor()
+        self.monitor2 = DellMonitor()
+        self.keyboard = DellKeyboard()
+        self.mouse = DellMouse()
+        self.monitor1.connect("VGA")
+        self.monitor2.connect("VGA")
+        self.keyboard.connect("USB")
+        self.mouse.connect("USB")
+
+    def printinfo(self):
+        print self.make
+        print self.model
+        print self.color
+        print self.connection
+        print self.owner
+
+    def printall(self):
+        self.printinfo()
+        self.monitor1.printinfo()
+        self.monitor2.printinfo()
+        self.keyboard.printinfo()
+        self.mouse.printinfo()
+
+
+workstation1 = Workstation()
+workstation1.printinfo()
+print "========"
+workstation1.printall()
+
+
